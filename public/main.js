a3ead8f0f5cfd09366ff99dcd1714877d360ccaa
Lagad Reshma <96974585+lagadreshma@users.noreply.github.com>
2023-02-26 09:01:40 +0530

    Add files via upload

---
+++ public/main.js
@@ -0,0 +1,10 @@
+// JavaScript for hide or show Menu Items
+let nav = document.getElementById("navlinks");
+
+function hide(){
+    nav.style.right = "0";
+}
+
+function show(){
+    nav.style.right = "-250px";
+}
